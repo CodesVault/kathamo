7ccfe1e0353e154e740f0a46e3c84c7cfda59088
AbmSourav <keramotul.islam@gmail.com>
2022-10-08 15:27:08 +0600

    version 0.0.5

--- howdy.php
+++ howdy.php
@@ -5,7 +5,7 @@
  * Plugin Name: Howdy
  * Plugin URI:
  * Description: A WordPress starter plugin.
- * Version: 0.0.3
+ * Version: 0.0.5
  * Author: CodesVault
  * Author URI: https://github.com/CodesVault
  * License: GPLv2 or later
@@ -17,7 +17,7 @@ use Howdy\Core\HowdyCore;
 if ( ! defined( 'ABSPATH' ) ) die();
 
 define( 'HOWDY', 'howdy' );
-define( 'HOWDY_VERSION', '0.0.3' );
+define( 'HOWDY_VERSION', '0.0.5' );
 define( 'HOWDY_FILE', __FILE__ );
 define( 'HOWDY_DIR_PATH', plugin_dir_path( HOWDY_FILE ) );
 define( 'HOWDY_PLUGIN_URL', plugins_url( '/', HOWDY_FILE ) );
