778c595caf613b09c8f17a3b882b853311ce84f1
Mahbub Hasan Imon <mahbubhasanimon@gmail.com>
2023-06-14 07:34:13 +0600

    Update config.php
    Internationalization

--- configs/config.php
+++ configs/config.php
@@ -10,7 +10,7 @@ return array(
 	'plugin_slug'		=> 'kathamo',
 	'namaspace_root'	=> 'Kathamo',
 	'plugin_version'	=> '1.0.0',
-	'plugin_name'		=> 'Kathamo',
+	'plugin_name'		=> __( 'Kathamo', 'kathamo' ),
 	'dev_mode'			=> true,
 	'root_dir'			=> dirname(__DIR__),
 	'middlewares'		=> [
