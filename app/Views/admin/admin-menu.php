dd0ea6d4792439b85565eabbf0c807b180bbd382
AbmSourav <keramotul.islam@gmail.com>
2023-06-15 12:35:50 +0600

    class name prefix changed

--- app/Views/admin/admin-menu.php
+++ app/Views/admin/admin-menu.php
@@ -1,4 +1,4 @@
-<h1 class="kathamo-title kf-text-3xl kf-text-green-800"><?php echo esc_html( $plugin_name ); ?></h1>
+<h1 class="kf-title kf-text-3xl kf-text-green-800"><?php echo esc_html( $plugin_name ); ?></h1>
 <p>
 	<?php echo esc_html( $developed ); ?>
 	<a class="kf-text-blue-700" class="" href="<?php echo esc_url( $author_link ); ?>">
